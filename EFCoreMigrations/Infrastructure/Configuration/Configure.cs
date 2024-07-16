11021b0e06cbf63a5d8c2a7ff82d5472010f6f90
alejograjal <alejandro.grajal.s@gmail.com>
2024-07-15 20:59:39 -0600

    Initial migrations

---
+++ EFCoreMigrations/Infrastructure/Configuration/Configure.cs
@@ -0,0 +1,11 @@
+// using Microsoft.Extensions.DependencyInjection;
+
+// namespace Infrastructure.Configuration;
+
+// public static class Configure
+// {
+//     public static void ConfigureInfrastructure(this IServiceCollection services)
+//     {
+//         services.AddTransient<IRepositoryStudent, RepositoryStudent>();
+//     }
+// }

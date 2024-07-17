874bff9c313df37da75008fefc1256191dd6c87a
Brayanvangarden <gerardo16@hotmail.com.ar>
2024-07-16 23:55:37 -0600

    Se crea carpeta Resources

---
+++ EFCoreMigrations/Resources/DBConfigurationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Configuration;
+
+public static class DBConfigurationExtension
+{
+    public static void ConfigureDataBase(this IServiceCollection services, IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(services); //excepciones si no hay servicio 
+
+        services.AddDbContext<EFCoreContext>(options => options.UseSqlServer(configuration.GetConnectionString("EFCoreDataBase"),
+             sqlServerOption =>
+             {
+                 sqlServerOption.EnableRetryOnFailure(); //si se desconecta volver a intentar 
+             })
+        );
+    }
+}
